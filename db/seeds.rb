8b2048c0e134c66ce513366c30147f373410a6b2
unknown <you@example.com>
2015-11-16 16:17:32 +0700

    Create project

---
+++ db/seeds.rb
@@ -0,0 +1,55 @@
+Product.delete_all
+Product.create!(title: 'Perfect Shape Bra',
+  description: 
+    %{<p>
+        The everyday go-to bra you love pairs push-up padding and the coverage you want with soft, sleek cups and a smoothing U-shaped back.<br>
+		Lift & Lining: Extreme lift, Full coverage underwire cups.<br>
+		Straps & Hooks: Fully adjustable straps can convert to crossback and snap into place for a secure hold, Back closure.<br>
+		Details & Fabric: Crisscross straps between cups, U-shaped ballet back prevents band from riding up and offers more coverage
+      </p>},
+  image_url:   'ruby/Perfect_Shape_Bra.jpg',    
+  price: 36.00)
+# . . .
+Product.create!(title: 'Add 2 Cups Push-Up Bra',
+  description:
+    %{<p>
+       	The ultimate lift-loving push-up is better than ever with even softer padding for the most cleavage and fullness.<br>
+		Lift & Lining: Maximum lift (adds 2 cups sizes), Underwire cups<br>
+		Straps & Hooks: Fully adjustable straps, Back closure, Double row of hook and eye closures, Can be worn classic or crossback<br>
+		Details & Fabric: U-shaped ballet back for an ultra-comfy fit, Imported nylon/spandex. Racer-back: polyester/spandex.
+      </p>},
+  image_url: 'ruby/Add_2_Cups_Push_Up_Bra.jpg',
+  price: 49.95)
+# . . .
+Product.create!(title: 'Unlined Demi Bra',
+  description:
+    %{<p>
+		A naturally sexy, unlined demi and soft cups make this low-cut bra your everyday go-to. Designed to disappear under everything.<br>
+		Lift & Lining: Unlined, Underwire cups.<br>
+		Straps & Hooks: Fully adjustable straps can convert to crossback and snap into place for a secure hold, Back closure.<br>
+		Details & Fabric: U-shaped ballet back prevents band from riding up and offers more coverage, VS logo at center front
+      </p>},
+  image_url: 'ruby/Unlined_Demi_Bra.jpg',
+  price: 41.50)
+# . . .
+Product.create!(title: 'Shimmer Bralette',
+  description:
+    %{<p>
+		Beautiful floral embroidery shines across this sheer shape, adorned with feminine ruffles and a delicate necklace.<br>
+		Lift & Lining: Unlined triangle cups, Wireless cups.<br>
+		Straps & Hooks: Adjustable straps, Back hook closure<br>
+		Details & Fabric: Allover shimmery, floral embroidery, Ruffle trim under cups, Imported nylon/polyester<br>
+      </p>},
+  image_url: 'ruby/Shimmer_Bralette.jpg',
+  price: 44.05)
+# . . .
+Product.create!(title: 'Cups Push-Up Bra',
+  description:
+    %{<p>
+		The push-up that gives you a little extra lift for serious cleavage, with Memory Fit padding that memorizes your curves, a concealed.<br>
+		Lift & Lining: Adds 1 1/2 cups for extreme lift, Foam-encased underwire cups for comfort<br>
+		Straps & Hooks: Back closure, Double row of hook and eye closures<br>
+		Details & Fabric: Shine finish with matte trim at the top of the cups,VS logo charm between the cups<br>
+      </p>},
+  image_url: 'ruby/Cups_Push_Up_Bra.jpg',
+  price: 50.99)
